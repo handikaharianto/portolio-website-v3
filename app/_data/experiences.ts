843c345f2d43e356d0ab6cf3e427ea106d783c0f
handikaharianto <handikaharianto01@gmail.com>
2026-07-05 23:37:41 -0400

    chore: add new experience

--- app/_data/experiences.ts
+++ app/_data/experiences.ts
@@ -8,14 +8,24 @@ type Experience = {
 
 export const experiences: Experience[] = [
   {
+    position: "Software Engineer (Volunteer)",
+    company: "One Community Global",
+    technologies: ["React", "MongoDB", "Node.js", "Express.js"],
+    period: "June 2026 - Present",
+    description: [
+      "Worked on the open-source Highest Good Network application using React, Express.js, Node.js & MongoDB.",
+      "Developed a job application analytics feature to compare completion times and volume of applications per role.",
+    ],
+  },
+  {
     position: "Freelance Web Developer",
     company: "Remote",
     technologies: ["Gatsby", "Next.js", "Tailwind CSS"],
     period: "May 2022 - May 2023",
     description: [
-      "Developed responsive websites for clients in big data and IT services companies with modern frontend frameworks like Next.js and Gatsby.",
-      "Constructed user-centered designs based on user requirements using Figma.",
-      "Collaborated closely with a copywriter to align the contents with web designs.",
+      "Delivered high-performing, responsive websites for Malaysian enterprise clients using Next.js and Gatsby.",
+      "Designed user-centered Figma prototypes based on client requirements.",
+      "Improved website content with copywriters, ensuring 100% alignment between visual design and messaging.",
     ],
   },
   {
@@ -26,15 +36,15 @@ export const experiences: Experience[] = [
       "React",
       "MongoDB",
       "Node.js",
-      "Express",
+      "Express.js",
       "Redux",
       "Sass",
       "Blueprint JS",
     ],
     description: [
-      "Developed a full stack web application for a chatbot-based automation system named Digital Workspace Assistant using React/Typescript, Express, Node.js, and MongoDB.",
-      "Implemented a multi-channel feature that allows businesses to create their own chatbot on both WhatsApp and Telegram platform.",
-      "Continued the development of custom data table to manage user data by implementing various functionalities, such as server-side pagination, context menu, etc.",
+      "Developed a chatbot-based automation system for Malaysian businesses in retail, e-commerce, manufacturing, etc. using React/TypeScript, Express.js, Node.js, MongoDB, Redux, and Sass.",
+      "Implemented a multi-channel feature, allowing users to seamlessly integrate chatbots across both WhatsApp and Telegram, designing REST API for tenant platforms, and building functional React components from Figma.",
+      "Implemented a Glide Data Grid to handle large-scale data, including lazy loading, context menu, search, etc.",
     ],
   },
 ];
